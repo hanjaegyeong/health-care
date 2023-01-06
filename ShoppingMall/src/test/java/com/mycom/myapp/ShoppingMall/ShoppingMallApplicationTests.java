5398f9d5a7cbfef43010736d52a8a8f43a6bb4cf
hanjaegyeong <hjg000223@naver.com>
2023-01-06 14:11:33 +0900

    feat: ShoppingMall 부트 프로젝트 생성

---
+++ ShoppingMall/src/test/java/com/mycom/myapp/ShoppingMall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp.ShoppingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
